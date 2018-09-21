e29e957e7bdc3c10a11376549b7ba8a10960631e
Shubham Agrawal <shub8968@gmail.com>
2018-09-21 22:05:12 +0530

    Update SearchArray.java

--- SearchArray.java
+++ SearchArray.java
@@ -9,6 +9,30 @@ package javacodesds;
  *
  * @author Shubham
  */
-public class SearchArray {
-    
+import java.util.*;
+public class LinearSearch{
+    public static int search(int arr [],int n)
+    {
+        for(int i= 0 ; i<n; i++)
+        {
+            if(arr[i]==n)
+            return i;
+        }
+        return -1;
+    }
+    public static void main(String [] args){
+       Scanner kb = new Scanner(System.in);
+       System.out.println("Enter size of array");
+       int  k = kb.nextInt();
+       kb.nextLine();
+       int  arr [] = new int [k];
+       System.out.println("Enter array element");
+       for(int i = 0; i < k; i++){
+           arr[i] = kb.nextInt();
+       }
+       System.out.println("Enter element to be search");
+       int n = kb.nextInt();
+       n=search(arr,n);
+       System.out.println("The index of element is"+n);
+    }
 }
